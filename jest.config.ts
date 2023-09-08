f4c4ffc6e943b4eb93d9e2ad7abbb4ab4004b567
cbl980226 <binglinchen98@gmail.com>
2023-09-08 14:49:19 +0800

    fix(test): eSM syntax is not allowed in a CommonJS module when 'verbatimModuleSyntax' is enabled

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,19 @@ export default {
   preset: './jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        // Note: We shouldn't need to include `isolatedModules` here because it's a deprecated config option in TS 5,
+        // but setting it to `true` fixes the `ESM syntax is not allowed in a CommonJS module when
+        // 'verbatimModuleSyntax' is enabled` error that we're seeing when running our Jest tests.
+        isolatedModules: true,
+        useESM: true
+      }
+    ]
   },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.mts'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: './coverage/curd',
   testMatch: [
